2ad68dd3b925f316166c65029d77fe8b6106eb37
HasiBul Islam ShanTo <hasib@nybsys.com>
2022-06-23 12:55:27 +0600

    another process added

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -15,12 +15,32 @@ internal static class Program
         var solutionDetail = @"[{'1':'A'},{'2':'B'},{'3':'C'},{'4':'D'}]".Deserialize();
         var answerDetail = @"[{'1':'A'},{'2':'Null'},{'3':'C'},{'4':'multiple'}]".Deserialize();
 
+        // take high memory
         var result = (from keyValuePair in solutionDetail
                       let matchedPair = answerDetail.FirstOrDefault(c => c.Key == keyValuePair.Key)
                       where string.Equals(keyValuePair.Value, matchedPair.Value, StringComparison.CurrentCultureIgnoreCase)
                       select keyValuePair).Count();
+        // take low memory
+        var anotherCalculation = CalculateResult(solutionDetail, answerDetail);
+        
 
         Console.WriteLine("Match value between 2 JSON Array \nResult: ");
         Console.WriteLine(result);
+        Console.WriteLine(anotherCalculation);
+    }
+    public static int CalculateResult(IDictionary<byte, string> value, IDictionary<byte, string> compare)
+    {
+        var result = 0;
+        foreach (var item in value)
+        {
+            if (compare.ContainsKey(item.Key))
+            {
+                if (item.Value.Equals(compare[item.Key]))
+                {
+                    result++;
+                }
+            }
+        }
+        return result;
     }
 }
