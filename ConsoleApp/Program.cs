69b266913736cb70335aece87fb1cf8ed6c456c0
HasiBul Islam ShanTo <hasib@nybsys.com>
2022-06-23 12:10:03 +0600

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+namespace ConsoleApp;
+
+internal static class Program
+{
+    public static IDictionary<byte, string> Deserialize(this string serializedJsonString)
+    {
+        var deserializeObj = JsonConvert.DeserializeObject<List<Dictionary<byte, string>>>(serializedJsonString);
+        var dictionary = new Dictionary<byte, string>(); deserializeObj?.ForEach(d => dictionary.Add((byte)d.FirstOrDefault().Key, d.FirstOrDefault().Value));
+        return dictionary;
+    }
+
+    private static void Main(string[] args)
+    {
+        var solutionDetail = @"[{'1':'A'},{'2':'B'},{'3':'C'},{'4':'D'}]".Deserialize();
+        var answerDetail = @"{'1':'A'},{'2':'Null'},{'3':'C'},{'4':'multiple'}".Deserialize();
+
+        var result = (from keyValuePair in solutionDetail
+                      let matchedPair = answerDetail.FirstOrDefault(c => c.Key == keyValuePair.Key)
+                      where string.Equals(keyValuePair.Value.ToString(), matchedPair.Value, StringComparison.CurrentCultureIgnoreCase)
+                      select keyValuePair).Count();
+
+        Console.WriteLine(result);
+    }
+}
